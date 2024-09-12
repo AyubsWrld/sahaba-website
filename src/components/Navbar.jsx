a8d0081b8d617bb001cf04be5b7a9682778294f6
unknown <ayubfmohammed@gmail.com>
2024-09-11 19:40:47 -0600

    new changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Buttons from './Buttons.jsx';
+
+export default function Navbar() {
+  return (
+    <>
+      <div>
+        <Buttons />
+      </div>
+    </>
+  );
+}
